70a96836ccfe49192fbab2ae841c9de7b8c8133d
Code1357 <61643130+Code1357@users.noreply.github.com>
2020-06-23 07:00:43 +0900

    node_mysql_001

---
+++ node_mysql/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const router = require('express').Router();
+const mysql = require('mysql');
+
+// mysqlに接続
+const con = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'test'
+});
+
+con.connect(function (err) {
+  if (err) throw err;
+  else console.log('接続できました');
+});
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+  res.send('はじめまして');
+});
+
+app.use('/', router);
+app.listen(app.get('port'), () => {
+  console.log(`port${app.get('port')}を監視しています`);
+});
